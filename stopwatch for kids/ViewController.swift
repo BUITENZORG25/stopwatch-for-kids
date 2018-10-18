a70904ed4338bf67501217e5d66415dab808d305
User <user@kerjalemburbagaikuda.local>
2018-10-18 13:57:17 +0700

    Initial Commit

---
+++ stopwatch for kids/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stopwatch for kids
+//
+//  Created by User on 18/10/18.
+//  Copyright © 2018 cahaya Ilahi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
